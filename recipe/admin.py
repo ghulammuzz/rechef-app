f452f6d3ff29983e30b8fc047fd38e55dfbe14fd
ghulammuzz <ghulamxiaomi@gmail.com>
2023-06-11 00:41:30 +0700

    add

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Recipe
 
-admin.site.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'image', 'user', 'view', 'fav')
+    list_editable = ('view', 'fav')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'description')
+    list_per_page = 25
+admin.site.register(Recipe,  RecipeAdmin)
