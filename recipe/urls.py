d8b6d2b99a9af7fd938e0e84c4f8fa7d8ef9682c
ghulammuzz <ghulamxiaomi@gmail.com>
2023-07-02 01:06:57 +0700

    test branch

--- recipe/urls.py
+++ recipe/urls.py
@@ -9,6 +9,6 @@ router.register(r'recipe', RecipeViewset, basename='recipe')
 
 urlpatterns = [
     path(r'', include(router.urls)),
-    path('favorite/', FavoriteView.as_view(), name='favorite'),
+    path('favorite/', FavoriteView.as_view(), name='favorite-recipe'),
     path('dashboard/', DashboardView.as_view(), name='dashboard')
 ]
